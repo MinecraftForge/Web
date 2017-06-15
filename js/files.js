65e6e8ee23d1058a4f249e181053c4ea39bc8fb7
Marvin Rösch <marvinroesch99@gmail.com>
2017-06-15 18:23:30 +0200

    Introduce a variable for the mobile breakpoint and fix various issues with the files site

---
+++ js/files.js
@@ -0,0 +1,31 @@
+/*
+ Implementation for various utilities on the files site.
+ */
+$(document).ready(function () {
+    $('[data-toggle=tooltip]').tooltipster({
+        theme: 'tooltipster-shadow',
+        position: 'bottom',
+        animation: 'grow'
+    });
+    $('[data-toggle=popup]').tooltipster({
+        theme: 'tooltipster-shadow',
+        position: 'bottom',
+        animation: 'grow',
+        contentAsHTML: true,
+        interactive: true
+    });
+    $('.download-list .download-links li').each(function () {
+        if ($(this).find(".info-tooltip").length > 0) {
+            var info = $(this).children(".info-tooltip");
+            $(this).children(".info-link").tooltipster('content', info.html());
+        }
+    });
+});
+
+window.onload = function() {
+    if (location.hostname != 'files.minecraftforge.net') {
+        var elems = document.getElementsByTagName('a');
+        for (var i = 0; i < elems.length; i++)
+            elems[i]['href'] = elems[i]['href'].replace('files.minecraftforge.net', location.hostname);
+    }
+};
