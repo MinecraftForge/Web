34624927d55b7c5c434a214fc028f4e87e35bdb3
Marvin Rösch <marvinroesch99@gmail.com>
2017-12-26 15:16:15 +0100

    Add Gulp tasks for generating deployable bundles for files site

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,15 @@ var sass = require('gulp-sass');
 var minifyCSS = require('gulp-csso');
 var concat = require('gulp-concat-multi');
 var minifyJS = require('gulp-uglify');
+var zip = require('gulp-zip');
+var rename = require('gulp-regex-rename');
+var clean = require('gulp-clean');
 
 gulp.task('css', function () {
     return gulp.src('./sass/**/*.scss')
         .pipe(sass())
         .pipe(minifyCSS())
-        .pipe(gulp.dest('./dist/css'))
+        .pipe(gulp.dest('./dist/css'));
 });
 
 gulp.task('js', function () {
@@ -17,7 +20,32 @@ gulp.task('js', function () {
         'docs.js': ['js/shared/**/*.js', 'js/docs.js']
     })
         .pipe(minifyJS())
-        .pipe(gulp.dest('./dist/js'))
+        .pipe(gulp.dest('./dist/js'));
+});
+
+gulp.task('clean-temp', function () {
+    return gulp.src('./dist/temp', {read: false}).pipe(clean());
+});
+
+gulp.task('bundle-files:copy-css', ['clean-temp', 'css'], function () {
+    return gulp.src('./dist/css/website_*.css').pipe(rename(/website_([A-Za-z]+)/, 'styles_$1')).pipe(gulp.dest('./dist/temp/css'));
+});
+
+gulp.task('bundle-files:copy-js', ['clean-temp', 'js'], function () {
+    return gulp.src('./dist/js/files.js').pipe(rename(/files/, 'merged')).pipe(gulp.dest('./dist/temp/js'));
+});
+
+gulp.task('bundle-files:copy-html', ['clean-temp'], function () {
+    return gulp.src('./templates/*.html').pipe(gulp.dest('./dist/temp/templates'));
+});
+
+gulp.task('bundle-files', ['clean-temp', 'bundle-files:copy-css', 'bundle-files:copy-js', 'bundle-files:copy-html'], function () {
+    return gulp.src('./dist/temp/**/*',
+        {
+            base: './dist/temp/'
+        })
+        .pipe(zip('files-bundle.zip'))
+        .pipe(gulp.dest('./dist/'));
 });
 
 gulp.task('watch', function () {
