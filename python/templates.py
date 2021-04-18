93f7a0ea06564a7b8009dbb4ca307ae7b7b4f695
cpw <cpw+github@weeksfamily.ca>
2021-04-18 15:46:16 -0400

    add in proper description

--- python/templates.py
+++ python/templates.py
@@ -1,10 +1,11 @@
 import datetime
+import itertools
 import pathlib
 
 import jinja2
 
 from mc_version import MCVer
-
+from metadata import Artifact
 
 @jinja2.contextfunction
 def show_classifier(context, mc_version, classifier):
@@ -18,6 +19,17 @@ def humanformatdate(dt: datetime.datetime):
     return jinja2.Markup(f'<script>document.write(dayjs.unix({dt.timestamp()}).fromNow())</script>')
 
 
+def get_artifact_description(artifact: Artifact, mc_version):
+    if mc_version:
+        hdr = (f'Downloads for {artifact.fullname()} for Minecraft {mc_version}',)
+        lines = (f'{t.capitalize()}: {v}' for t, v in artifact.promotions[mc_version].items())
+    else:
+        hdr = (f'Downloads for {artifact.fullname()}',)
+        lines = (f'Latest: {artifact.all_versions[-1].version}',)
+
+    return '\n'.join(itertools.chain(hdr, lines))
+
+
 class Templates:
     def __init__(self, template_path: pathlib.Path, static_base, web_base, repository_base):
         self.env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_path))
@@ -26,6 +38,7 @@ class Templates:
         self.env.globals['repository_base'] = repository_base
         self.env.globals['now'] = datetime.datetime.utcnow()
         self.env.globals['show_classifier'] = show_classifier
+        self.env.globals['get_artifact_description'] = get_artifact_description
 
         self.env.filters['humanformatdate'] = humanformatdate
         self.env.filters['formatdate'] = lambda dt: f'{dt:%Y-%m-%d %H:%M:%S}'
